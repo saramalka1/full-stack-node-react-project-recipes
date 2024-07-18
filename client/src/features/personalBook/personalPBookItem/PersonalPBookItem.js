c9125c65015ee6f9b6dc0dd8e5bb032528c60af8
saramalka1 <sara325952430@gmail.com>
2024-07-18 10:10:38 +0300

    add personal book features

---
+++ client/src/features/personalBook/personalPBookItem/PersonalPBookItem.js
@@ -0,0 +1,152 @@
+import './personal-pbook-item.css'
+
+import { useParams } from 'react-router-dom'
+import useGetFilePath from '../../../hooks/useGetFilePath'
+import { FaClock } from "react-icons/fa"; import { FaConciergeBell } from "react-icons/fa";
+import { CgAdidas } from "react-icons/cg";
+import useAuth from '../../../hooks/useAuth'
+import { FiEdit } from "react-icons/fi";
+import { MdOutlineDelete } from "react-icons/md";
+import { useGetPBookItemMutation } from '../../personalBook/personalBookApiSlice'
+import { useEffect, useState } from 'react';
+import { RxStarFilled } from 'react-icons/rx';
+
+
+const PersonalPBookItem = () => {
+    const { isAdmin, _id } = useAuth()
+    const { objid } = useParams()
+
+    const [getPBookItem, { data, isSuccess, isLoading, isError, error }] = useGetPBookItemMutation()
+    const [recipeobj, setrecipeobj] = useState()
+    const [doneIngredients, setDoneIngredients] = useState({})
+    const [doneInstructions, setDoneInstructions] = useState({})
+    const { getFilePath } = useGetFilePath()
+
+    useEffect(() => {
+        if (objid)
+            getPBookItem(objid)
+    }, [objid])
+
+    useEffect(() => {
+        if (isSuccess && data.data)
+            setrecipeobj(data.data)
+    }, [isSuccess, data])
+
+    const handleIngredientClick = (index) => {
+        setDoneIngredients(prevState => ({
+            ...prevState,
+            [index]: !prevState[index]
+        }))
+    }
+
+    const handleInstructionClick = (index) => {
+        setDoneInstructions(prevState => ({
+            ...prevState,
+            [index]: !prevState[index]
+        }))
+    }
+
+    if (isError)
+        return <h1>משהו קרה בצד שלנו {JSON.stringify(error.message)}</h1>
+    if (isLoading) return
+    if (!recipeobj)
+        return
+
+    return (
+        <div className='pb'>
+            <div className='pbook-item'>
+                <div className='single-recipec-page-container'>
+
+                    <div className='single-recipec-page-sub-container'>
+                        <div className='recipe-head-container'>
+                            <div className='recipe-head-info'>
+                                <div className='recipe-name-and-description'>
+                                    <div className='recipe-name'>{recipeobj.recipe.name}</div>
+                                    <div className='recipe-description'>{recipeobj.recipe.description}</div>
+                                </div>
+                                <div className='recipe-from-and-date'>
+                                    <div className='recipe-from'>{`מאת:${recipeobj.recipe.writeruser.name} `}</div>
+                                    <div className='recipe-date'>{` בתאריך :(${recipeobj.recipe.createdAt}) `}</div>
+                                </div>
+                            </div>
+                            <div className='recipe-head-img-container'>
+                                <img src={getFilePath(recipeobj.recipe.imgurl)} />
+                            </div>
+                        </div>
+                        <div className='recipe-icons-ingredientd-and-instructions-container'>
+
+                            <div className='recipe-time-count-type-level-container'>
+                                <div className='time-container'>
+                                    <FaClock className='icon' />
+                                    <div>
+                                        {recipeobj.recipe.preparationtime}
+                                    </div>
+                                </div>
+                                <div className='count-container'>
+                                    <FaConciergeBell className='icon' />
+                                    <div>
+                                        {`${recipeobj.recipe.amount} מנות`}
+                                    </div>
+                                </div>
+                                <div className='type-container' id={recipeobj.recipe.type}>
+                                    <RxStarFilled className='icon' />
+                                    {recipeobj.recipe.type === 'PARVE' && 'פרווה'}
+                                    {recipeobj.recipe.type === 'DAIRY' && 'חלבי'}
+                                    {recipeobj.recipe.type === 'MEAT' && 'בשרי'}
+                                </div>
+                                <div className='level-container' id={recipeobj.recipe.level}>
+                                    <CgAdidas className='icon' />
+                                    {recipeobj.recipe.level === 'HARD' && 'קשה'}
+                                    {recipeobj.recipe.level === 'EASY' && 'קל'}
+                                    {recipeobj.recipe.level === 'MEDIUM' && 'בינוני'}
+                                </div>
+                                {/* אם יש משתמש ניתן אפשרות של הוספה לספר המתכונים */}
+
+
+                            </div>
+                            <div className='recipe-ingredients-and-instructions'>
+                                <div className='recipe-ingredients-container'>
+                                    <div className='ingredients-title'>אז מה צריך :</div>
+                                    {recipeobj.recipe.ingredients.map((ing, index) => (
+                                        <div className={`single-ingredient ${doneIngredients[index] ? 'done' : ''}`} key={index}>
+                                            <input
+                                                className='chekbox-done'
+                                                type='checkbox'
+                                                checked={doneIngredients[index] || false}
+                                                onChange={() => handleIngredientClick(index)}
+                                            />
+                                            {`${index + 1}. ${ing}`}
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className='recipe-instructions-container'>
+                                    <div className='instructions-title'>הוראות הכנה :</div>
+                                    {recipeobj.recipe.instructions.map((inst, index) => (
+                                        <div className={`single-instruction ${doneInstructions[index] ? 'done' : ''}`} key={index}>
+                                            <input
+                                                type='checkbox'
+                                                checked={doneInstructions[index] || false}
+                                                onChange={() => handleInstructionClick(index)}
+                                            />
+                                            {`${index + 1}. ${inst}`}
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className='wish-and-actions'>
+
+
+                                    <div className='wish'>
+                                        בתיאבון !
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default PersonalPBookItem
